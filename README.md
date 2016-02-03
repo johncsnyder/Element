6b7b56e3f4c7d09e4f717f2834cb8e647eea21b6
John Snyder <johncsnyder@gmail.com>
2016-02-03 06:35:48 +0100

    added automatic installation of external library libxc using BinDeps

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
-# Element
+
+
+## Element.jl
 
 [![Build Status](https://travis-ci.org/johncsnyder/Element.jl.svg?branch=master)](https://travis-ci.org/johncsnyder/Element.jl)
+
+
+
+Library for electronic structure calculations in Julia.
+
+
+Installation via `Pkg.clone("https://github.com/johncsnyder/Element.jl.git")`.
+
+
+[Documentation](http://johncsnyder.github.io/Element.jl/)
+
--- deps/build.jl
+++ deps/build.jl
@@ -1,7 +1,50 @@
+
+
+using BinDeps
+using Compat
+
+@BinDeps.setup
+
+libxc = library_dependency("libxc")
+
+@osx_only begin
+	using Homebrew
+	provides(Homebrew.HB, "libxc", libxc, os = :Darwin)
+end
+
+provides(AptGet, "libxc", libxc)
+provides(Yum, "libxc-devel", libxc)
+
+julia_usrdir = normpath(JULIA_HOME*"/../") # This is a stopgap, we need a better built-in solution to get the included libraries
+libdirs = AbstractString["$(julia_usrdir)/lib"]
+includedirs = AbstractString["$(julia_usrdir)/include"]
+
+provides(
+	Sources,
+	URI("http://www.tddft.org/programs/octopus/down.php?file=libxc/libxc-2.2.2.tar.gz"),
+	libxc
+)
+
+provides(
+	BuildProcess,
+	Autotools(lib_dirs = libdirs, include_dirs = includedirs),
+	libxc
+)
+
+
+
+@compat @BinDeps.install Dict(:libxc => :libxc)
+
+
+
+
+
 using Element: generate_lebedev
 
 # generate lebedev angular grids
 generate_lebedev()
+
+
 
 
 
